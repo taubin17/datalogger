50002cbb4fd274388b7ccb8b0abba008063bf1f5
Tyler David Aubin <tyler.aubin@maine.edu>
2020-04-30 16:14:28 -0400

    Added homework 7

--- SQLite.py
+++ SQLite.py
@@ -11,15 +11,16 @@ parsed = data.split()
 
 
 #Set each field of the table to its respective result from split()
-day_of_week = parsed[0]
-month = parsed[1]
-day = parsed[2]
-time_now = parsed[3]
-year = parsed[4]
-temperature = parsed[5]
-humidity = parsed[6]
-pressure = parsed[7]
-light = parsed[8]
+my_date = parsed[0]
+my_time = parsed[1]
+high_res_temperature = parsed[2]
+humidity = ((float(parsed[3])) / 1000) #Convert from millipercent
+pressure = ((float(parsed[4])) / 10000000)#Convert from hundreds of micro bar to bar
+light = parsed[5]
+IR_intensity = parsed[6]
+Full_Spectrum = parsed[7]
+Visible_Light = parsed[8]
+low_res_temperature = ((float(parsed[9])) / 100)
 
 #Confirm the current data has successfully entered SQL script
 print(parsed)
@@ -28,9 +29,9 @@ connection = sqlite3.connect('SensorData.db')
 #Create cursor to control database
 cursor = connection.cursor()
 #Create our table if it doesn't exist
-cursor.execute('CREATE TABLE IF NOT EXISTS date_time (day_of_week TEXT, month TEXT, day TEXT, time TEXT, year TEXT, temperature FLOAT, humidity FLOAT, pressure FLOAT, light INT)')
+cursor.execute('CREATE TABLE IF NOT EXISTS date_time (date date, time time, high_res_temperature FLOAT, humidity FLOAT, pressure FLOAT, light INT, IR_intensity INT, Full_Spectrum INT, Visible_Light INT, low_res_temperature FLOAT)')
 #Take the data value held in the file and upload it, separating each element of time, data, and the date
-cursor.execute('INSERT INTO date_time VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)', (day_of_week, month, day, time_now, year, temperature, humidity, pressure, light))
+cursor.execute('INSERT INTO date_time VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (my_date, my_time, high_res_temperature, humidity, pressure, light, IR_intensity, Full_Spectrum, Visible_Light, low_res_temperature))
 #IF our entries tally more than 1400 at any time, starting at the bottom of the stack (SQL IS LIFO), remove beginning elements
 cursor.execute('DELETE FROM date_time WHERE ROWID IN (SELECT ROWID FROM date_time ORDER BY ROWID DESC LIMIT -1 OFFSET 1439)')
 connection.commit()
--- serialread.py
+++ serialread.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import time, datetime
+from datetime import date
 import serial
 
 ser = serial.Serial(
@@ -15,52 +16,64 @@ ser = serial.Serial(
 print(ser.name)
 
 
-#Grab data forever
-while True:
-    #Open the file here to write data out
-    fd = open('datalogger.dat', 'w')
+#Open the file here to write data out
+fd = open('datalogger.dat', 'w')
     
-    #Send a "character" to serial to initialize data. Also track the time we recieve data back
-    print("Start : %s" % time.ctime())
-    ser.write('Start\n')
-    x = ser.readline()
-    #Parse the data to be split by spaces, rather than by pipes.
-    
-    x = x.replace(' |', '')
-    #Split data by each sensor result
-    parsed = x.split()
-    
-    #Grab the sensor results we want
-    data = [parsed[0], parsed[7], parsed[6], parsed[4]]
-    
-    #Confirm the data is correct
-    print data
+#Send a "character" to serial to initialize data. Also track the time we recieve data back
+print("Start : %s" % time.ctime())
+ser.write('Start\n')
+time.sleep(0.100)
+ser.flushInput()
+x = ser.readline()
+ser.flushOutput()
 
-    #Write to data file the date and time
-    fd.write(time.ctime()),
+#Parse the data to be split by spaces, rather than by pipes.
+x = x.replace(' |', '')
+
+#Split data by each sensor result
+parsed = x.split()
     
-    #Write all sensor values seperated by spaces to data file
-    fd.write(" "),
-    fd.write(data[0]),
-    fd.write(" "),
-    fd.write(data[1]),
-    fd.write(" "),
-    fd.write(data[2]),
-    fd.write(" ")
-    fd.write(data[3]),
-    fd.write(" ")
+#Grab the sensor results we want
+data = [parsed[0], parsed[7], parsed[6], parsed[4], parsed[1], parsed[2], parsed[3], parsed[5]]
     
-    #Write a new line after each data and date/time entry
-    fd.write("\n")
-    fd.close() 
-    #exec(open("SQLite.py").read())
-    #Wait a minute to repeat the loop
-    time.sleep(1)
-    print("End : %s" % time.ctime())
-    exec(open("SQLite.py").read())
-    print
-    print
+#Confirm the data is correct
+print data
+
+#Write to data file the date and time
+grab_time = time.ctime()
+parse_time = grab_time.split()
+my_time = parse_time[3]
+string_date = str(date.today())
+
+fd.write(string_date),
+fd.write(" "),
+fd.write(my_time),
+
+#Write all sensor values seperated by spaces to data file
+fd.write(" "),
+fd.write(data[0]),
+fd.write(" "),
+fd.write(data[1]),
+fd.write(" "),
+fd.write(data[2]),
+fd.write(" ")
+fd.write(data[3]),
+fd.write(" "),
+fd.write(data[4]),
+fd.write(" "),
+fd.write(data[5]),
+fd.write(" "),
+fd.write(data[6]),
+fd.write(" "),
+fd.write(data[7]),
+fd.write(" "),
+
     
+#Write a new line after each data and date/time entry
+fd.write("\n")
+fd.close() 
+exec(open("SQLite.py").read())
+
 #Cleanup file descriptors
 fd.close()
 #Leave
